bd449ee994853726139793a6f6f5a44b745b7512
Alex Werner <alex@werner.fr>
2019-09-03 18:47:34 +0200

    fix(tests): updated tests to await

--- src/FSLock/methods/add.js
+++ src/FSLock/methods/add.js
@@ -1,5 +1,5 @@
 const Job = require('../../Job/Job');
-module.exports = async function add(command, path, params){
+module.exports =  function add(command, path, params){
     const job = new Job({command, path, params});
     this.queue.push(job);
     job.state = 'queued';
--- tests/FSLock/FSLock.js
+++ tests/FSLock/FSLock.js
@@ -13,8 +13,8 @@ describe('FSQueue',  function suite() {
     expect(queue.queue).to.deep.equal([]);
     expect(queue.options.autoexec).to.equal(false);
   });
-  it('should create a command and return a job element', function () {
-    const addedJob = queue.add('Directory.exists', usersPath);
+  it('should create a command and return a job element', async function () {
+    const addedJob = await queue.add('Directory.exists', usersPath);
     expect(addedJob.constructor.name).to.equal(Job.name);
     expect(addedJob.state).to.equal('queued');
     expect(queue.queue.length).to.equal(1);
@@ -26,7 +26,7 @@ describe('FSQueue',  function suite() {
     expect(job.state).to.equal('queued');
   });
   it('should process a command and mutate job element', async function () {
-    const job2 = queue.add('Directory.create', usersPath);
+    const job2 = await queue.add('Directory.create', usersPath);
 
     await queue.processNext();
     expect(job.state).to.equal('executed');
@@ -36,7 +36,7 @@ describe('FSQueue',  function suite() {
     expect(job2.state).to.equal('executed');
     expect(job2.results).to.equal(true);
 
-    const job3 = queue.add('Directory.exists', usersPath);
+    const job3 = await queue.add('Directory.exists', usersPath);
     await queue.processNext();
     expect(job3.state).to.equal('executed');
     console.log(job3)
@@ -46,8 +46,8 @@ describe('FSQueue',  function suite() {
     const doc = {"_id": "5d6d4123117055fa0b17bb15", "email": "jean@valjean.fr", "age": 27}
     const {_id, email, age} = doc;
 
-    const createJob = queue.add('File.create', `${usersPath}/${doc._id}.json`, Object.assign({}, {_id, email}));
-    const readJob = queue.add('File.read', `${usersPath}/${doc._id}.json`);
+    const createJob = await queue.add('File.create', `${usersPath}/${doc._id}.json`, Object.assign({}, {_id, email}));
+    const readJob = await queue.add('File.read', `${usersPath}/${doc._id}.json`);
 
     // We process
     await queue.processNext();
@@ -58,35 +58,37 @@ describe('FSQueue',  function suite() {
     const storeDoc = readJob.results;
     expect(storeDoc).to.deep.equal(Object.assign({}, {_id, email}));
 
-    const updateJob = queue.add('File.create', `${usersPath}/${doc._id}.json`, Object.assign({}, storeDoc, {age}))
+    const updateJob = await queue.add('File.create', `${usersPath}/${doc._id}.json`, Object.assign({}, storeDoc, {age}))
     await queue.processNext();
 
-    const verifyJob = queue.add('File.read', `${usersPath}/${doc._id}.json`);
+    const verifyJob = await queue.add('File.read', `${usersPath}/${doc._id}.json`);
     await queue.processNext();
 
     expect(verifyJob.results).to.deep.equal(doc);
   });
-  it('should have job emitting a event when ready', function (done) {
+  it('should have job emitting a event when ready', async function () {
     const doc = {"_id": "5d6d4123117055fa0b17bb16", "email": "alex@valjean.fr", "age": 27}
 
-    queue.add('File.create', `${usersPath}/${doc._id}.json`, doc)
-    const readJob = queue.add('File.read', `${usersPath}/${doc._id}.json`);
-    console.log(readJob);
-    readJob.on('executed', () => {
-      expect(readJob.state).to.deep.equal('executed');
-      expect(readJob.results).to.deep.equal(doc);
-      done();
+    await queue.add('File.create', `${usersPath}/${doc._id}.json`, doc)
+    const readJob = await queue.add('File.read', `${usersPath}/${doc._id}.json`);
+    return new Promise(async (res, rej)=>{
+      readJob.on('executed', () => {
+        expect(readJob.state).to.deep.equal('executed');
+        expect(readJob.results).to.deep.equal(doc);
+        res(true);
+      })
+
+      await queue.processAll();
     })
 
-    queue.processAll();
 
   });
   it('should allow await of execution of a job', async function () {
     const doc = {"_id": "5d6d4123117055fa0b17bb17", "email": "victor@valjean.fr", "age": 20}
     autoQueue = new FSLock();
 
-    autoQueue.add('File.create', `${usersPath}/${doc._id}.json`, doc)
-    const readJob = autoQueue.add('File.read', `${usersPath}/${doc._id}.json`);
+    await autoQueue.add('File.create', `${usersPath}/${doc._id}.json`, doc)
+    const readJob = await autoQueue.add('File.read', `${usersPath}/${doc._id}.json`);
 
     await readJob.execution();
 
