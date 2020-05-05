a9016a0657cadd2a1d02ea7ee03bc95e4def8bb0
Alex Werner <alex@werner.fr>
2020-05-05 12:33:55 +0200

    impr: proper warning on incorrect command

--- src/FSLock/methods/ops/execCommand.js
+++ src/FSLock/methods/ops/execCommand.js
@@ -6,6 +6,12 @@ module.exports = async function execCommand(command, path, params){
   let result, error;
   try{
     const [type,fn] = command.split('.');
+    if(!utils[type]){
+      throw new Error(`Not handled type ${type} - Expected one of ${Object.keys(utils)}`)
+    }
+    if(!utils[type][fn]){
+      throw new Error(`Not handled method ${type}.${fn} - Expected one of ${Object.keys(utils[type])}`)
+    }
     result = await utils[type][fn](path, params);
   } catch(e){
     error = e;
