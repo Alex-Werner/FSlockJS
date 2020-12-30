01761b7f9648d712ecf7a77057bcab6d3a5a0699
Alex Werner <alex@werner.fr>
2020-12-31 00:07:17 +0100

    docs: add usage example

---
+++ examples/usage.js
@@ -0,0 +1,15 @@
+const FSLock = require('../src/FSLock/FSLock');
+const queue = new FSLock({
+  autoexec: true
+});
+
+(async ()=>{
+  // Will auto-process this as autoexec is true.
+  const createDirJob = queue.add('Directory.create','./myfolder');
+
+  const createFileJob = queue.add('File.create', './myfolder/myfile.json', { something: true });
+  await createFileJob.execution();
+  const file = await queue.add('File.read', './myfolder/myfile.json').execution();
+
+  console.log(file.result);
+})()
